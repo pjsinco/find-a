3d4e1b7b6a400fc1aaeb84f934b3f6fbaf7f6112
pjsinco <psinco@fas.harvard.edu>
2014-12-08 17:57:41 -0600

    adds comments to nba-2.js

--- components/js/nba-2.js
+++ components/js/nba-2.js
@@ -15,20 +15,29 @@
     console.log(arr);
     return arr;
   }
+  
 
   var nbaTeams = new Bloodhound({
 
     datumTokenizer: function(d) {
+      // note: the text property is what we want to search through
       return Bloodhound.tokenizers.whitespace(d.text);
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     prefetch: {
+      // example of nba-2.json, which we call below
+      //  {
+      //      "team": "Dallas Mavericks",
+      //      "player": "Dirk"
+      //  },
       url: './js/nba-2.json',
       filter: function(d) {
+        // let's make a new text property to show in our autocomplete list
         return $.map(d, function(e) {
           return {
             team: e.team,
             player: e.player,
+            // text: will be what we display in the autocomplete list
             text: e.team + ', ' + e.player,
           }
         });
@@ -38,13 +47,15 @@
   });
 
   nbaTeams.initialize();
-  //nbaTeams.clearPrefetchCache();
+  nbaTeams.clearPrefetchCache(); // do this when we're debugging!
 
   $('#nba .typeahead').typeahead({
     highlight: true
   }, {
     name: 'nba-teams',
-    displayKey: 'text',
+    // displayKey is the property we want to show in our autcomplete list;
+    // so we're showing the text property of the datum
+    displayKey: 'text', 
     source: nbaTeams.ttAdapter()
   });
 
